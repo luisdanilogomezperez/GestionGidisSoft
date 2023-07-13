ee6fd2323f8cdb65de08ba104804790e8b1ae3c5
luisdanilogomezperez <luisdanilogp@ufps.edu.co>
2023-07-12 21:50:17 -0500

    first commit

---
+++ src/main/java/com/GestionGidisSoft/GestionGidisSoftApplication.java
@@ -0,0 +1,13 @@
+package com.GestionGidisSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionGidisSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionGidisSoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GestionGidisSoft/GestionGidisSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GestionGidisSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionGidisSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
